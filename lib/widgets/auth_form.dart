76f0ddccc572c03558ac31cd72074b8fee2b5614
jessicacorrea <jessicamilenacorrea@gmail.com>
2023-08-14 12:10:16 -0300

    Criando animaçao manual

--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -13,12 +13,44 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   AuthMode _authMode = AuthMode.Login;
   final passwordController = TextEditingController();
   final Map<String, String> authData = {'email': '', 'password': ''};
   final formKey = GlobalKey<FormState>();
   bool isLoading = false;
+  AnimationController? animationController;
+  Animation<Size>? heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 300,
+      ),
+    );
+
+    heightAnimation = Tween(
+      begin: Size(double.infinity, 310),
+      end: Size(double.infinity, 400),
+    ).animate(
+      CurvedAnimation(
+        parent: animationController!,
+        curve: Curves.linear,
+      ),
+    );
+
+    heightAnimation?.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController?.dispose();
+  }
 
   bool isLogin() => _authMode == AuthMode.Login;
   bool isSignup() => _authMode == AuthMode.Signup;
@@ -87,8 +119,10 @@ class _AuthFormState extends State<AuthForm> {
     setState(() {
       if (isLogin()) {
         _authMode = AuthMode.Signup;
+        animationController?.forward();
       } else {
         _authMode = AuthMode.Login;
+        animationController?.reverse();
       }
     });
   }
@@ -102,7 +136,7 @@ class _AuthFormState extends State<AuthForm> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Container(
         padding: const EdgeInsets.all(16),
-        height: isLogin() ? 320 : 400,
+        height: heightAnimation?.value.height ?? (isLogin() ? 320 : 400),
         width: deviceSize.width * 0.75,
         child: Form(
           key: formKey,
